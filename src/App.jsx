67572e04c23933f10278606a3c805fdc52613f1b
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-04-26 14:06:45 +0330

    craete router with new method with install react-router-dom@6 from npm package and define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+//createBrowserRouter dar router version 6 hastesh vaghti az in estefade konim
+//mishe azash bara load dardan data ha va ersal data az tarigh submit form ,...
+//estefade kard dar hali k ba ruter ghadin inkara nemishe
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+//in functioni hast k ma hame masir haru dakhelesh tarif mikonim
+//ba arraye e az obj ha ,k har obj ye masir hastesh
+//bara estefade az natayej ono dar ye moteghayer zakhire mikonim
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  //bara barresi ye sefaresh mojod ba params
+  { path: "/order/:orderId", element: <Order /> },
+]);
+
 function App() {
-  return <div>fast-react-pizza</div>;
+  //bara estefade az router miyaym routerProvider ru tarif mikonim va esm moteghayer router hamono
+  //b onvane props besh midim
+  return <RouterProvider router={router} />;
 }
 
 export default App;
