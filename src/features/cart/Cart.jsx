22a89619002d21a652bc5259f78da45c910f6eef
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-08-03 13:02:53 +0330

    complated username in all component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Button from '../../ui/Button';
 import LinkButton from '../../ui/LinkButton';
@@ -29,12 +30,13 @@ const fakeCart = [
 
 function Cart() {
   const cart = fakeCart;
+  const username = useSelector((state) => state.user.username);
 
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="devide-stone-200 mt-3 divide-y border-b">
         {cart.map((item) => (
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../srvices/apiRestaurant';
 import Button from '../../ui/Button';
@@ -34,6 +35,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username);
   //az in estefade mikonim k vaghti dare ersal mikone dokme ersal khamosh bashe
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
@@ -66,7 +68,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
